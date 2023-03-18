b1ede440b469f86706173bea5e3c121d7fd72c23
RAMIRO <ragahe@gmail.com>
2023-03-17 23:56:30 -0300

    Repaso de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,5 +2,13 @@
 
 int main(){
     printf("HOLA MUNDO");
+    int a, *punt;
+    punt=&a;
+    a=1;
+    printf("\nEl contenido del puntero es: %d", *punt);
+    printf("\nLa direccion de memoria almacenada por el puntero es: %p", punt);
+    printf("\nla direccion de memoria de la variable es: %p", &a);
+    printf("\nla direccion de memoria del puntero. es: %p", &punt);
+    printf("\nel tamanio de memoria utilizado por esa variable es: %d", sizeof(a));
     return 0;
 }
